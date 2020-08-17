8bdfa12b5d637cac9e21601dbffd669277915d11
Nextige <nextigesoft@gmail.com>
2020-08-17 11:33:43 +0530

    upadted project in git.

---
+++ assets/js/app.js
@@ -0,0 +1,61 @@
+// testimonials slider
+let swiper2 = new Swiper('.testimonials-wrapper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: false,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+// Sticky Header
+// sticky header
+$(window).scroll(function () {
+    var st = $(this).scrollTop();
+    if (st > 50) {
+        $("header").addClass("sticky");
+    } else {
+        $("header").removeClass("sticky");
+    }
+});
+
+
+// smooth scroll
+$(".btn-groups a+a").bind("click", function (e) {
+    e.preventDefault();
+
+    var target = $(this).attr("href");
+
+    $("html, body")
+        .stop()
+        .animate({
+                scrollTop: $(target).offset().top - 30,
+            },
+            600,
+            function () {
+                location.hash = target + 30;
+            }
+        );
+
+    return false;
+});
+
+
+// Loader
+$(window).on('load', function () {
+    $(".loader").fadeOut(1500);
+});
+
+// wow animation
+wow = new WOW({
+    boxClass: "wow", // default
+    animateClass: "animated", // default
+    offset: 0, // default
+    mobile: false, // default
+    live: true, // default
+});
+wow.init();
