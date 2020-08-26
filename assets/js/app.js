fa96353998bbe9efe3e75740b41e48d2c1fc9ab8
KK <krishnkantpatidar@gmail.com>
2020-08-26 17:09:34 +0530

    added parallex effect and fixed couple of requested changes.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 // testimonials slider
 let swiper2 = new Swiper('.testimonials-wrapper', {
     slidesPerView: 1,
+    autoHeight: true,
     spaceBetween: 30,
     loop: false,
     pagination: {
@@ -58,3 +59,46 @@ wow = new WOW({
     live: true, // default
 });
 wow.init();
+
+// parallax 
+function initParallax() {
+    'use strict';
+    var parallaxElements = $('.parallax'),
+        parallaxQuantity = parallaxElements.length;
+    $(window).on('scroll', function () {
+        window.requestAnimationFrame(function () {
+            parallaxElements.each(function () {
+                var $this = $(this),
+                    $speed = $this.attr('data-parallax-speed');
+                for (var i = 0; i < parallaxQuantity; i++) {
+                    var $scrolled = $(window).scrollTop() - $this.offset().top + $this.height();
+                    $this.css({
+                        'transform': 'translate3d(0,' + $scrolled * $speed + 'px, 0)'
+                    });
+                }
+            });
+        });
+    });
+}
+$('.parallax').each(function () {
+    'use strict';
+    var $this = $(this),
+        $src = $this.attr('data-image-src'),
+        $wrapperHeight = $this.parents('.parallax-wrapper').find('.parallax-container').outerHeight();
+    $this.parents('.full-blocks').addClass('parallax-block');
+    $this.css('background-image', 'url(' + $src + ')');
+    $this.parents('.parallax-wrapper').outerHeight($wrapperHeight);
+});
+$(window).resize(function () {
+    'use strict';
+    $('.parallax-section').each(function () {
+        var $newHeight = $('.parallax-container', this).outerHeight();
+        $('.parallax-wrapper', this).height($newHeight);
+    });
+});
+(function ($) {
+    'use strict';
+    if ($('.parallax').length) {
+        initParallax();
+    }
+}($));
